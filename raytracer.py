0b0c16c2b9199fbdb7824defdd4c91fbb45fb536
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2013-11-26 00:34:55 +0100

    Testing out pygame, nothing special

---
+++ raytracer.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from math import *
+import pygame, sys
+from pygame.locals import *
+from vector import Vector
+
+
+def main():
+   pygame.init()
+   background=pygame.display.set_mode((600,600))
+   #Simple drawing test atm
+   while True:
+      for i in range(255):
+         for j in range(255):
+            background.set_at((i,j), 0xFFFFFF)
+      pygame.display.update()
+
+if __name__=="__main__":
+   main()
