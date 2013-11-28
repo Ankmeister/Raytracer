d2d52139e2c354fbc8c1f8a0ca5a172896ead569
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2013-11-28 09:48:56 +0100

    fler vektorfunktioner

--- vector.py
+++ vector.py
@@ -5,14 +5,24 @@ class Vector:
       self.x = x
       self.y = y
       self.z = z
+      self.magnitude = self.getMagnitude()
 
-   def length(self):
+   def getMagnitude(self):
       return (self.x**2 + self.y ** 2  + self.z ** 2)**0.5
+
+   def getMag2(self):
+      return self.x**2 + self.y ** 2  + self.z ** 2     
 
    def setScale(self, scale):
       self.x *= scale
       self.y *= scale
       self.z *= scale
+
+   def unitVec(self):
+      self.x /= self.magnitude
+      self.y /= self.magnitude
+      self.z /= self.magnitude
+   
 
    def __add__(self,v1):
       vsum = Vector(self.x + v1.x, self.y + v1.y, self.z + v1.z)
@@ -24,7 +34,10 @@ class Vector:
 
    def __mul__(self,v1):
       return self.x*v1.x + self.y*v1.y + self.z*v1.z
-   
+
+   def __neg__(self):
+      return Vector(-self.x, -self.y, -self.z)
+
 def crossp(v1, v2):
    cross.x = v1.y*v2.z - v1.z*v2.y
    cross.y = v1.z*v2.x - v1.x*v2.z
