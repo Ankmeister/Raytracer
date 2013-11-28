fc5b3ccf38ba2d5164adcfb818e3ce88441cd184
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2013-11-28 09:47:59 +0100

    initial sphere class

---
+++ Sphere.py
@@ -0,0 +1,16 @@
+import vector
+
+class Sphere:
+   def __init__(self,rad, position):
+      self.direction = direction
+      self.position = position
+
+   def setPosition(self,x,y,z):
+      self.position = vector.Vector(x,y,z)
+
+   def getPosition(self):
+      return self.position
+   
+   def setRad(self,x,y,z):
+      self.rad=vector.Vector(x,y,z)
+
