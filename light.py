3fc405780809f153c8f5058d484caa4a8d18b80c
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2014-04-07 20:10:52 +0200

    Cleanup

--- light.py
+++
@@ -1,6 +0,0 @@
-from vector import *
-
-class Light:
-   def __init__(self,origin, color):
-      self.origin = origin
-      self.color = color
--- ray.py
+++
@@ -1,8 +0,0 @@
-from vector import *
-
-class Ray:
-   def __init__(self,direction, origin):
-      """t is the scalefactor at the ray's hitpoint with a sphere"""
-      self.direction = direction.norm()
-      self.origin = origin
-      self.t = 0
